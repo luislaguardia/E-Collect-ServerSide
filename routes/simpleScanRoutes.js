67c78b5b257b6ead14a5bc227df01714dc902a35
awit salvatore <laguardialuisgabriel@gmail.com>
2025-07-31 02:15:57 +0800

    few changes

--- routes/simpleScanRoutes.js
+++ routes/simpleScanRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const simpleScanController = require('../controllers/simpleScanController');
+// const simpleScanController = require('../controllers/simpleScanController');
 const { authMiddleware } = require('../middleware/authMiddleware');
+
+const { scanQR, getHistory } = require('../controllers/simpleScanController');
 
 // if want, put back the middleware "authMiddleware"
 
-router.post('/scan', authMiddleware, simpleScanController.scanQR);
-
+router.post('/scan', authMiddleware, scanQR);
+router.get('/history', authMiddleware, getHistory);
 // router.get('/activity', simpleScanController.getActivity);
 // router.get('/rewards', simpleScanController.getRewards);
 // router.post('/redeem/:rewardId', simpleScanController.redeemReward);
